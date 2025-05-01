5c077f392708be8eeb51024216ec9d539e198dca
Pedro Santos <pedrosts.dev@gmail.com>
2025-05-01 14:28:21 -0300

    H2 database, test profile, JPA

--- src/main/java/com/pedrosts/dev/course/resources/UserResource.java
+++ src/main/java/com/pedrosts/dev/course/resources/UserResource.java
@@ -1,4 +1,18 @@
 package com.pedrosts.dev.course.resources;
 
+import com.pedrosts.dev.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findALl() {
+        User u = new User(1L, "Pedro", "pedro@gmail.com", "1234567", "1234");
+        return ResponseEntity.ok().body(u);
+    }
 }
