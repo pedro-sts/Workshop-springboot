bf14d69a92571935efeb55e310ea991e39691d21
Pedro Santos <pedrosts.dev@gmail.com>
2025-05-08 18:13:07 -0300

    Constraint Violation exception

--- src/main/java/com/pedrosts/dev/course/services/UserService.java
+++ src/main/java/com/pedrosts/dev/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.pedrosts.dev.course.services;
 
 import com.pedrosts.dev.course.entities.User;
 import com.pedrosts.dev.course.repositories.UserRepository;
+import com.pedrosts.dev.course.services.exceptions.DatabaseException;
 import com.pedrosts.dev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User updateUser(Long id, User user) {
